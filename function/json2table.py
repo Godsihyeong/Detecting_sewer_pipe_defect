5f5b169249b8ab834929afe32dd1ba3689a2a3e9
Godsihyeong <105179996+Godsihyeong@users.noreply.github.com>
2024-01-30 14:06:22 +0900

    Add files via upload

---
+++ function/json2table.py
@@ -0,0 +1,39 @@
+import pandas as pd
+
+def convert_bbox(bbox,width,height):
+    dw = 1.0 / width
+    dh = 1.0 / height
+    x = bbox[0] + bbox[2] / 2.0
+    y = bbox[1] + bbox[3] / 2.0
+    w = bbox[2]
+    h = bbox[3]
+    return [x * dw, y * dh, w * dw, h * dh]
+
+def open_optimized(filename, file_list):
+    # 필요한 컬럼 정의
+    keys = ['id', 'image_id', 'category_id', 'area', 'bbox']
+
+    # 이미지와 어노테이션 데이터를 DataFrame으로 변환
+    images_df = pd.DataFrame(filename["images"])
+    annotations_df = pd.DataFrame(filename["annotations"])
+
+    # file_list에 해당하는 이미지 ID 추출
+    images_df = images_df[images_df["file_name"].isin(file_list)]
+    image_ids = images_df["id"].tolist()
+
+    # 해당하는 이미지 ID에 대한 어노테이션 필터링
+    filtered_annotations1 = annotations_df[annotations_df["image_id"].isin(image_ids)]
+    filtered_annotations1 = filtered_annotations1[keys]
+    selected= images_df[['id', 'width', 'height']]
+    # A와 selected_cols_B 데이터프레임을 "id"와 "id" 열을 기준으로 병합
+    filtered_annotations = filtered_annotations1.merge(selected, left_on='image_id', right_on='id', how='inner')
+    filtered_annotations["bbox"] = filtered_annotations.apply(lambda row: convert_bbox(row["bbox"], row["width"], row["height"]), axis=1)
+
+
+
+    # bbox 컬럼을 분할
+    bbox_df = pd.DataFrame(filtered_annotations["bbox"].tolist(), columns=['x', 'y', 'w', 'h'])
+    filtered_annotations = pd.concat([filtered_annotations, bbox_df], axis=1)
+    filtered_annotations.drop('bbox', axis=1, inplace=True)
+
+    return filtered_annotations
---
+++ function/making_txt.py
@@ -0,0 +1,39 @@
+import os
+import pandas as pd
+from tqdm import tqdm
+
+def df2txt(df, data, file_class, class_number):
+    df.reset_index(drop=True, inplace=True)  # index 재설정하고 기존 index 제거
+
+    folder_path = '{}_folder'.format(file_class)
+    current_directory = 'D:/sewer detect/valid_folder'
+
+    # 폴더 생성
+    folder_path_full = os.path.join(current_directory, folder_path)
+    os.makedirs(folder_path_full, exist_ok=True)
+
+    data1 = pd.DataFrame(data["images"])
+
+    current_img_id = df.iloc[0]["image_id"]
+    file_path = os.path.join(folder_path_full, data1[data1["id"] == current_img_id]["file_name"].values[0].split('.')[0] + ".txt")
+
+    with tqdm(total=len(df), desc="Processing") as pbar:
+        with open(file_path, "w", encoding="utf8") as file:
+            for i, row in df.iterrows():
+                img_id = row["image_id"]
+                file_name = data1[data1["id"] == img_id]["file_name"].values[0]
+
+                if img_id != current_img_id:
+                    file.close()
+
+                    # 새로운 파일 경로 설정
+                    file_path = os.path.join(folder_path_full, file_name.split('.')[0] + ".txt")
+                    file = open(file_path, "w", encoding="utf8")
+
+                row_content = str(class_number) + ' ' + ' '.join(str(row[z]) for z in ["x", "y", "w", "h"])
+                file.write(row_content + '\n')
+
+                current_img_id = img_id
+                pbar.update(1)
+
+            
